a543a4fd1cdb3dcaec5b9b46e5d30d5e558ca1a4
Utasi Bence <mac@Utasi-MacBook-Air.local>
2022-10-09 16:25:24 +0200

    init

---
+++ components/GoalInput.js
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from 'react-native';
+
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText('');
+  }
+
+  return (
+    <Modal visible={props.visible} animationType='slide'>
+      <View style={styles.inputContainer}>
+        <Image
+          source={require('../assets/images/goal.png')}
+          style={styles.image}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder='Your course goal'
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title='Cancel' onPress={props.onCancel} color='#f31282' />
+          </View>
+          <View style={styles.button}>
+            <Button title='Add goal' onPress={addGoalHandler} color='#b180f0' />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#311b6b',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#e4d0ff',
+    width: '100%',
+    padding: 16,
+    backgroundColor: '#e4d0ff',
+    color: '#120438',
+    borderRadius: 6,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+});
+
+export default GoalInput;
